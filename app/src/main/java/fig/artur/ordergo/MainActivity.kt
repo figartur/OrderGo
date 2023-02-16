2b4b8d310c02c807fd464e420313b02d12df02bc
figartur <97570942+figartur@users.noreply.github.com>
2023-02-16 16:38:50 +0000

    REGISTER AND LOGIN DONE, START OF MAIN ACTIVITY

--- app/src/main/java/fig/artur/ordergo/MainActivity.kt
+++ app/src/main/java/fig/artur/ordergo/MainActivity.kt
@@ -3,29 +3,50 @@ package fig.artur.ordergo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import fig.artur.ordergo.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth : FirebaseAuth
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
         auth = FirebaseAuth.getInstance()
+
+        setContentView(binding.root)
+
+        binding.navigationBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.search -> replaceFragment(SearchFragment())
+                R.id.my_store -> replaceFragment(MyStoreFragment())
+                R.id.orders -> replaceFragment(OrdersFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+
+                else ->{}
+            }
+            true
+        }
 
         if(auth.currentUser == null){
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
 
-        btn_logout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
+    private fun replaceFragment(fragment: Fragment){
 
-            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
-            finish()
-        }
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
